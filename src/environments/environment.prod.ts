820953bcedc08bdd525e6401495ada03f78bf989
mana <mana99@hotmail.fr>
2022-12-14 16:12:22 +0100

    ajout barre type d'events + card event responsive

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+    production: true,
+    AdresseAPI :"http://localhost:3000/",
+    appVersion: require('../../package.json').version,
+    nomEnvironnement: "local",
+    timeToken : 0.00347222
+};
