ccb5db22e714dd933af91747d532695ff3300502
julieep <julie.emerard@sfr.fr>
2023-01-04 17:43:54 +0100

    refactor: 💡 delete unused input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FileUploadModule } from '@iplab/ngx-file-upload';
 import { CookieService } from 'ngx-cookie-service';
 import { AdminComponent } from './admin/admin.component';
-import { InputComponent } from './components/input/input.component';
 import { SvgComponent } from './components/svg/svg.component';
 
 @NgModule({
@@ -37,7 +36,6 @@ import { SvgComponent } from './components/svg/svg.component';
     ErrorComponent,
     EspaceUtilisateurComponent,
     AdminComponent,
-    InputComponent,
     SvgComponent
   ],
   imports: [
