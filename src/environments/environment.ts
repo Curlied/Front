be387f9866fea2128015795ac623d8e1fef63dff
jemerardprete <julie.emerard@sfr.fr>
2023-01-05 08:57:01 +0100

    ci: 🎡 add single quotes to fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,8 @@
-
 export const environment = {
   production: false,
-  AdresseAPI :"http://localhost:3000/",
+  AdresseAPI: 'http://localhost:3000/',
   appVersion: require('../../package.json').version,
-  nomEnvironnement: "local",
-  timeToken : 0.00347222,
-  AdresseIcon:"./assets/images/icons.svg#"
+  nomEnvironnement: 'local',
+  timeToken: 0.00347222,
+  AdresseIcon: './assets/images/icons.svg#',
 };
-
