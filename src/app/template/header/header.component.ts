21f58a65e8f6261ba47664d3393b0b4b87dc218c
BeatlesBOB <beatlesbob74120@gmail.com>
2023-05-07 17:04:24 +0200

    Update header.component.ts

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -22,7 +22,9 @@ export class HeaderComponent implements OnInit {
   }
 
   handleCloseMenu() {
-    new bootstrap.Collapse(this.menuToggle.nativeElement);
+    if (window.matchMedia('(max-width: 991.98px)').matches) {
+      new bootstrap.Collapse(this.menuToggle.nativeElement);
+    }
   }
 
   logout() {
